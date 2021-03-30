3d1c5ac8eedfcbfe6c28392f570381e709d05be8
KarinaGanieva-sl <46797639+KarinaGanieva-sl@users.noreply.github.com>
2021-03-30 17:36:27 +0300

    Update pdf_to_image.py

--- pdf_to_image.py
+++ pdf_to_image.py
@@ -11,12 +11,11 @@ from os.path import isfile, join
 def convert_pdf_to_jpeg(folder_name):
     images_folder_name = 'images/'
     files = [f for f in listdir(folder_name) if isfile(join(folder_name, f))]
-    i = 0
     for file in files:
-        images = convert_from_path(file, poppler_path=r"C:\Program Files\Release-21.03.0\poppler-21.03.0\Library\bin")
-        j = 0
+        print(file)
+        images = convert_from_path(folder_name+'/'+file, poppler_path=r"C:\Program Files\Release-21.03.0\poppler-21.03.0\Library\bin")
+        i = 0
         for im in images:
-            im.save(images_folder_name + str(i) + '/' + str(j) + '.JPEG')
-            j += 1
-        i += 1
+            im.save(images_folder_name + str(i) + '.jpg')
+            i += 1
     return images_folder_name
